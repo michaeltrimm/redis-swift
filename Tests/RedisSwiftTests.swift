9add1581d65207ab6e1e1731f9a01849ee59970a
Michael Trimm <michael@michaeltrimm.com>
2016-12-19 12:10:57 -0500

    Implemented sample unit tests for the class

--- Tests/RedisSwiftTests.swift
+++ Tests/RedisSwiftTests.swift
@@ -6,25 +6,49 @@
 //
 //
 
+import Redis
+
 class RedisSwiftTests : XCTestCase {
+    
+    let redis: Redis
+    let testKey = "myFancyKey"
+    let testValue = "something super awesome here"
     
     override func setUp(){
         super.setUp()
+        
+        redis = Redis(address: "localhost", port: 6379)
+        
+        XCTAssert(redis != nil)
     }
     
     func testSetKey(){
+        
+        let set:Bool = Redis.set(keyName: self.testKey, value: self.testValue)
+        
+        XCTAssert(set == true)
         
     }
     
     func testGetKey(){
         
+        let get:String? = Redis.get(keyName: self.testKey)
+        
+        XCTAssert(get == testValue)
+        
     }
     
     func testDeleteKey(){
         
+        let del: Bool = Redis.delete(keyName: self.testKey)
+        
+        XCTAssert(del == true)
+        
     }
     
     override func tearDown(){
+        
+        // nothing to tear down
         
     }
     
