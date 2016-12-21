52cd5f00588d1152e3bf1218598c96177507f031
Michael Trimm <michael@michaeltrimm.com>
2016-12-21 09:18:14 -0500

    Added HeaderDoc comments and copyright notice to all swift files

--- Sources/RedisSwift.swift
+++ Sources/RedisSwift.swift
@@ -1,10 +1,24 @@
-//
-//  Package.swift
-//  RedisSwift
-//
-//  Created by Michael A. Trimm on 12/19/16.
-//
-//
+/**
+ * Copyright (c) 2016 Michael A Trimm
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 import Foundation
 import cURL
@@ -15,48 +29,134 @@ public class Redis {
     public let server: String
     public var port: Int
     
+    /**
+     Redis client interface
+     
+     - Author:
+     Michael A. Trimm <michael@michaeltrimm.com>
+     
+     - parameters:
+        - address: host address of redis server
+        - port: listening port for the redis instance connecting to
+    */
     public init(address: String = "localhost", port: Int = 6379){
-        self.server = address
-        self.port = port
+        server = address
+        port = port
     }
     
-    public func get(keyName: String) -> String? {
+    /**
+     Get a stored value by its key
+    
+     - Author:
+     Michael A. Trimm <michael@michaeltrimm.com>
+ 
+     - parameters:
+        - keyName: Name of the key stored in redis
+ 
+     - throws:
+     An error of type PerformError should the cURL call fail
+     
+     - returns:
+     If the key exists, return its value, otherwise return nil
+    */
+    public func get(keyName: String) throws -> String? {
         let cmd = "GET \(keyName)"
         
         do {
-            let (headerData, bodyData) = try self.performAction(command: cmd)
+            
+            let (headerData, bodyData) = try performAction(command: cmd)
+            
+            return String(bytes: bodyData, encoding: .utf8)!
+            
         } catch PerformError.curlError {
             return nil
         }
     }
     
-    public func set(keyName: String, value: String) -> Bool {
+    /**
+     Set (or create) a new record in Redis
+     
+     - Author:
+     Michael A. Trimm <michael@michaeltrimm.com>
+     
+     - parameters:
+        - keyName: Name to retrieve this key->value pair
+        - value: Value to store in the key->value pair 
+     
+     - throws:
+     An error of type PerformError should the cURL call fail
+     
+     - returns:
+     true or false depending on if the set was successful
+    */
+    public func set(keyName: String, value: String) throws -> Bool {
         let cmd = "SET \(keyName) \(value)"
         
         do {
-            let (headerData, bodyData) = try self.performAction(command: cmd)
+            
+            let (headerData, bodyData) = try performAction(command: cmd)
+            let body:String = String(bytes: bodyData, encoding: .utf8)!
+            
+            return (body == "+OK" || body == "+OK\n")
+            
         } catch PerformError.curlError {
             return false
         }
     }
     
+    /**
+     Delete a stored key from the Redis server
+     
+     - Author:
+     Michael A. Trimm <michael@michaeltrimm.com>
+     
+     - parameters:
+        - keyName: Name of the key that should be deleted
+     
+     - throws:
+     An error of type PerformError should the cURL call fail
+     
+     - returns:
+     true or false depending on if the key was actually deleted
+     (if the key does not exist, this will return true)
+    */
     public func delete(keyName: String) -> Bool {
         let cmd = "DEL \(keyName)"
         
         do {
-            let (headerData, bodyData) = try self.performAction(command: cmd)
+            let (headerData, bodyData) = try performAction(command: cmd)
         } catch PerformError.curlError {
             return false
         }
     }
     
+    /**
+     Extending the Error type for class-specific error responses
+    */
     public enum PerformError : Error {
         case curlError(Int, Data, Data)
     }
     
+    /**
+     Responsible for creating the cURL object, inserting the command, executing the
+     command and formatting the response
+     
+     - Author: 
+     Michael A. Trimm <michael@michaeltrimm.com>
+     
+     - parameters:
+        - command: Full redis command to be executed
+        - verbose: Boolean toggle to display debug data from cURL call 
+     
+     - throws:
+     An error of type PerformError should the cURL call fail
+     
+     - returns:
+     tuple of cURL response header data and body data
+    */
     private func performAction(command: String, verbose: Bool = false) throws -> (responseHeaderData, responseBodyData) {
         
-        let url = "tcp://\(self.server):\(self.port)"
+        let url = "tcp://\(server):\(port)"
         let curl = CURL(url: url)
         
         curl.setOption(CURLOPT_UPLOAD, int: 1)
--- Tests/RedisSwiftTests.swift
+++ Tests/RedisSwiftTests.swift
@@ -1,10 +1,24 @@
-//
-//  Package.swift
-//  RedisSwift
-//
-//  Created by Michael A. Trimm on 12/19/16.
-//
-//
+/**
+ * Copyright (c) 2016 Michael A Trimm
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 import Redis
 
@@ -24,7 +38,7 @@ class RedisSwiftTests : XCTestCase {
     
     func testSetKey(){
         
-        let set:Bool = self.redis.set(keyName: self.testKey, value: self.testValue)
+        let set:Bool = redis.set(keyName: testKey, value: testValue)
         
         XCTAssert(set == true)
         
@@ -32,7 +46,7 @@ class RedisSwiftTests : XCTestCase {
     
     func testGetKey(){
         
-        let get:String? = self.redis.get(keyName: self.testKey)
+        let get:String? = redis.get(keyName: testKey)
         
         XCTAssert(get == testValue)
         
@@ -40,7 +54,7 @@ class RedisSwiftTests : XCTestCase {
     
     func testDeleteKey(){
         
-        let del: Bool = self.redis.delete(keyName: self.testKey)
+        let del: Bool = redis.delete(keyName: testKey)
         
         XCTAssert(del == true)
         
